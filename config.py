455c3fc7979ca6e1a66f65f91a91613e7e803872
garantia <renanhs80@gmail.com>
2020-09-17 19:00:42 -0300

    Ajustes em templates e db local.

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'cfa.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
 SECRET_KEY = '4NBn148Y4VkzFnN6o8Gk'
 #SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') # Para docker (heroku)
 #SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 #app.config['SECRET_KEY'] = SECRET_KEY
--- consistente/__init__.py
+++ consistente/__init__.py
@@ -1,10 +1,10 @@
 import os
+
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
 
 app = Flask(__name__)
 app.config.from_object('config')
